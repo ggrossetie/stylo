e70d7a26d46963d5f874b2e997223dd4a98d60f1
Guillaume Grossetie <ggrossetie@yuzutech.fr>
2025-06-03 14:26:49 +0200

    Améliore la lisibilité des stats

--- front/src/components/ArticleStats.jsx
+++ front/src/components/ArticleStats.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
+import { Trans, useTranslation } from 'react-i18next'
 import { shallowEqual, useSelector } from 'react-redux'
 import styles from './articleStats.module.scss'
-import { useTranslation } from 'react-i18next'
 
-export default function ArticleStats() {
+export default function ArticleStats () {
   const { t } = useTranslation()
   const articleStats = useSelector((state) => state.articleStats, shallowEqual)
   return (
     <ul className={styles.stats} aria-label={t('article.stats.menuLabel')}>
-      <li>{t('article.stats.words', { count: articleStats.wordCount })}</li>
       <li>
-        {t('article.stats.chars', {
-          count: articleStats.charCountNoSpace,
-          countWithSpaces: articleStats.charCountPlusSpace,
-        })}
+        <Trans i18nKey="article.stats.words" count={articleStats.wordCount}>
+          <span className={styles.count}>123</span> words
+        </Trans>
       </li>
       <li>
-        {t('article.stats.citations', { count: articleStats.citationNb })}
+        <Trans i18nKey="article.stats.chars" count={articleStats.charCountNoSpace} values={{ countWithSpaces: articleStats.charCountPlusSpace }}>
+          <span className={styles.count}>123</span> characters <span className={styles.count}>123</span> with spaces
+        </Trans>
+      </li>
+      <li>
+        <Trans i18nKey="article.stats.citations" count={articleStats.citationNb}>
+          <span className={styles.count}>123</span> citations
+        </Trans>
       </li>
     </ul>
   )
